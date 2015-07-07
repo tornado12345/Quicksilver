6cf2724377fa0637cb94c091ce230dcdf42e5149
Etienne Samson <samson.etienne@gmail.com>
2015-07-07 23:23:57 +0200

    Don't unconditionally allow entries to be edited in debug

--- Quicksilver/Code-App/QSCatalogPrefPane.m
+++ Quicksilver/Code-App/QSCatalogPrefPane.m
@@ -321,11 +321,6 @@ static id _sharedInstance;
 }
 
 - (BOOL)selectedCatalogEntryIsEditable {
-	
-#ifdef DEBUG
-	return YES;
-#endif
-	
 	id source = [currentItem source];
 	if ([source respondsToSelector:@selector(usesGlobalSettings)] && [source performSelector:@selector(usesGlobalSettings)])
 		return YES;
